dd3da70783761627a52e747ce77eeb007cc0a385
sgutierrezos04 <sgutierrezos@unal.edu.co>
2023-02-19 16:37:39 -0500

    1er envio

---
+++ main.py
@@ -0,0 +1,43 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    def cambiarColor(self, color):
+        if color =="amarillo" or color =="rojo" or color =="verde" or color =="blanco" or color =="negro":
+            self.color = color
+
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    def cantidadAsientos(self):
+        cantidadA = 0
+        for i in self.asientos:
+            if type(i) == Asiento:
+                cantidadA += 1
+        return cantidadA
+    def verificarIntegridad(self):
+        if self.motor.registro == self.registro:
+            for i in self.asientos:
+                if self.i.registro != self.registro:
+                    return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        if tipo == "gasolina" or tipo == "electrico":
+            self.tipo = tipo
